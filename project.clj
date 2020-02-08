f18db46c61696b3e0cdc82bf27777fd7e86f5f4d
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-02-08 05:06:53 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License 2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]
-                 [reagent "0.9.0-rc2"]
-                 [cljsjs/material-ui "4.5.1-0"]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [reagent "0.9.1"]
+                 [cljsjs/material-ui "4.8.3-0"]
                  [cljsjs/material-ui-pickers "3.2.7-0"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]
-                 [camel-snake-kebab "0.4.0"]]
-  :plugins [[lein-cljfmt "0.6.4"]]
+                 [camel-snake-kebab "0.4.1"]]
+  :plugins [[lein-cljfmt "0.6.6"]]
   :source-paths ["src/core" "src/icons"]
   :profiles {:dev {:dependencies   [[com.bhauman/figwheel-main "0.2.3"]
                                     [prismatic/dommy "1.1.0"]
