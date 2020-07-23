0557f76d5711868a8411bccdf2f65d26501f4ead
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-07-23 14:23:06 +0300

    Update example project

--- example/project.clj
+++ example/project.clj
@@ -1,8 +1,8 @@
 (defproject reagent-material-ui-example "1.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.597"]
+                 [org.clojure/clojurescript "1.10.773"]
                  [reagent "0.10.0"]
-                 [arttuka/reagent-material-ui "4.9.12-0"]]
+                 [arttuka/reagent-material-ui "4.11.0-0"]]
   :source-paths ["src/common"]
   :profiles {:dev {:dependencies   [[com.bhauman/figwheel-main "0.2.4" :exclusions [org.clojure/clojurescript]]]
                    :resource-paths ["target"]}}
