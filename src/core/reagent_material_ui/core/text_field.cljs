a25bb282541c9f9f7da9df87cbe052a9b6b4cba0
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-11-29 00:45:35 +0200

    Remove undefined vals from textfield input props

--- src/core/reagent_material_ui/core/text_field.cljs
+++ src/core/reagent_material_ui/core/text_field.cljs
@@ -6,18 +6,23 @@
             [reagent.core :as r]
             ["@material-ui/core/TextField" :as MuiTextField]
             [reagent-material-ui.core.textarea-autosize :refer [react-textarea-autosize]]
-            [reagent-material-ui.util :refer [adapt-react-class get-anycase assoc-anycase js->clj']]))
+            [reagent-material-ui.util :refer [adapt-react-class get-anycase assoc-anycase js->clj' remove-undefined-vals]]))
 
 ;; Fix cursor positioning for Reagent
 ;; https://github.com/reagent-project/reagent/blob/master/doc/examples/material-ui.md
 
+(defn ^:private input-props [props ref]
+  (-> (js->clj' props)
+      (assoc :ref ref)
+      (remove-undefined-vals)))
+
 (def ^:private input
   (forward-ref input [props ref]
-    (r/as-element [:input (assoc (js->clj' props) :ref ref)])))
+    (r/as-element [:input (input-props props ref)])))
 
 (def ^:private textarea
   (forward-ref textarea [props ref]
-    (r/as-element [:textarea (assoc (js->clj' props) :ref ref)])))
+    (r/as-element [:textarea (input-props props ref)])))
 
 (def ^:private mui-text-field (adapt-react-class (or (.-default MuiTextField) (.-TextField MuiTextField)) "mui-text-field"))
 
