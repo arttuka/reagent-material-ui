49e7777d2a0ce3e805b9eb33711fcebcb7bce2ea
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2021-04-10 14:14:00 +0300

    Update example project

--- example/src/example/core.cljs
+++ example/src/example/core.cljs
@@ -8,13 +8,15 @@
             [reagent-material-ui.core.chip :refer [chip]]
             [reagent-material-ui.core.css-baseline :refer [css-baseline]]
             [reagent-material-ui.core.input-adornment :refer [input-adornment]]
-            [reagent-material-ui.core.grid :refer [grid]]
             [reagent-material-ui.core.menu-item :refer [menu-item]]
+            [reagent-material-ui.core.stack :refer [stack]]
+            [reagent-material-ui.core.styled-engine-provider :refer [styled-engine-provider]]
             [reagent-material-ui.core.text-field :refer [text-field]]
             [reagent-material-ui.core.toolbar :refer [toolbar]]
             [reagent-material-ui.icons.add-box :refer [add-box]]
             [reagent-material-ui.icons.clear :refer [clear]]
             [reagent-material-ui.icons.face :refer [face]]
+            [reagent-material-ui.icons.face-outlined :refer [face-outlined]]
             [reagent-material-ui.lab.date-picker :refer [date-picker]]
             [reagent-material-ui.lab.localization-provider :refer [localization-provider]]
             [reagent-material-ui.styles :as styles]
@@ -34,7 +36,9 @@
              :secondary colors/green}})
 
 (defn custom-styles [{:keys [spacing] :as theme}]
-  {:button     {:margin (spacing 1)}
+  {:container  {:margin-left (spacing 8)
+                :align-items :flex-start}
+   :button     {:margin (spacing 1)}
    :text-field {:width        200
                 :margin-left  (spacing 1)
                 :margin-right (spacing 1)}})
@@ -46,119 +50,112 @@
 (defonce date-picker-state (r/atom nil))
 (defonce autocomplete-state (r/atom nil))
 
-(defn form [{:keys [classes] :as props}]
-  [grid
-   {:container true
-    :direction "column"
-    :spacing   2}
+(defn form* [{:keys [classes] :as props}]
+  [stack {:direction "column"
+          :spacing   2
+          :class     (:container classes)}
 
-   [grid {:item true}
-    [toolbar
-     {:disable-gutters true}
-     [button
-      {:variant  "contained"
-       :color    "primary"
-       :class    (:button classes)
-       :on-click #(swap! text-state str " foo")}
-      "Update value property"
-      [add-box]]
+   [toolbar {:disable-gutters true}
+    [button
+     {:variant  "contained"
+      :color    "primary"
+      :class    (:button classes)
+      :on-click #(swap! text-state str " foo")}
+     "Update value property"
+     [add-box]]
+    [button
+     {:variant  "outlined"
+      :color    "secondary"
+      :class    (:button classes)
+      :on-click (fn []
+                  (reset! text-state "")
+                  (reset! select-state 1)
+                  (reset! date-picker-state nil)
+                  (reset! autocomplete-state nil))}
+     "Reset"
+     [clear]]]
 
-     [button
-      {:variant  "outlined"
-       :color    "secondary"
-       :class    (:button classes)
-       :on-click (fn []
-                   (reset! text-state "")
-                   (reset! select-state 1)
-                   (reset! date-picker-state nil)
-                   (reset! autocomplete-state nil))}
-      "Reset"
-      [clear]]]]
+   [text-field
+    {:value       @text-state
+     :label       "Text input"
+     :placeholder "Placeholder"
+     :helper-text "Helper text"
+     :class       (:text-field classes)
+     :on-change   (fn [e]
+                    (reset! text-state (event-value e)))}]
 
-   [grid {:item true}
-    [text-field
-     {:value       @text-state
-      :label       "Text input"
-      :placeholder "Placeholder"
-      :helper-text "Helper text"
-      :class       (:text-field classes)
-      :on-change   (fn [e]
-                     (reset! text-state (event-value e)))}]]
+   [text-field
+    {:value       @text-state
+     :label       "Text with Adornment"
+     :placeholder "Placeholder"
+     :helper-text "Helper text"
+     :class       (:text-field classes)
+     :on-change   (fn [e]
+                    (reset! text-state (event-value e)))
+     :InputProps  {:end-adornment (r/as-element [input-adornment {:position "end"} "Baz"])}}]
 
-   [grid {:item true}
-    [text-field
-     {:value       @text-state
-      :label       "Text with Adornment"
-      :placeholder "Placeholder"
-      :helper-text "Helper text"
-      :class       (:text-field classes)
-      :on-change   (fn [e]
-                     (reset! text-state (event-value e)))
-      :InputProps {:end-adornment (r/as-element [input-adornment {:position "end"} "Baz"])}}]]
+   [text-field
+    {:value       @text-state
+     :label       "Textarea"
+     :placeholder "Placeholder"
+     :helper-text "Helper text"
+     :class       (:text-field classes)
+     :on-change   (fn [e]
+                    (reset! text-state (event-value e)))
+     :multiline   true
+     :rows        10}]
 
-   [grid {:item true}
-    [text-field
-     {:value       @text-state
-      :label       "Textarea"
-      :placeholder "Placeholder"
-      :helper-text "Helper text"
-      :class       (:text-field classes)
-      :on-change   (fn [e]
-                     (reset! text-state (event-value e)))
-      :multiline   true
-      :rows        10}]]
+   [text-field
+    {:id        :textarea-autosize
+     :value     @text-state
+     :label     "Autosized textarea"
+     :class     (:text-field classes)
+     :on-change (fn [e]
+                  (reset! text-state (event-value e)))
+     :multiline true
+     :min-rows  2
+     :max-rows  10}]
 
-   [grid {:item true}
-    [text-field
-     {:id        :textarea-autosize
-      :value     @text-state
-      :label     "Autosized textarea"
-      :class     (:text-field classes)
-      :on-change (fn [e]
-                   (reset! text-state (event-value e)))
-      :multiline true
-      :min-rows  2
-      :max-rows  10}]]
+   [text-field
+    {:value       @select-state
+     :label       "Select"
+     :placeholder "Placeholder"
+     :helper-text "Helper text"
+     :class       (:text-field classes)
+     :on-change   (fn [e]
+                    (reset! select-state (event-value e)))
+     :select      true}
+    [menu-item
+     {:value 1}
+     "Item 1"]
+    [menu-item
+     {:value 2}
+     "Item 2"]]
 
-   [grid {:item true}
-    [text-field
-     {:value       @select-state
-      :label       "Select"
-      :placeholder "Placeholder"
-      :helper-text "Helper text"
-      :class       (:text-field classes)
-      :on-change   (fn [e]
-                     (reset! select-state (event-value e)))
-      :select      true}
-     [menu-item
-      {:value 1}
-      "Item 1"]
-     [menu-item
-      {:value 2}
-      "Item 2"]]]
+   ;; For properties that require React Node as parameter,
+   ;; use r/as-element to convert Reagent hiccup forms into React elements,
+   ;; or use r/create-element to directly instantiate element from React class (i.e. non-adapted React component).
+   [stack {:direction :row
+           :spacing   2}
+    [chip {:icon  (r/as-element [face])
+           :label "Icon element example, r/as-element"}]
+    [chip {:icon  (r/as-element [face-outlined])
+           :label "Another example icon"}]]
 
-   [grid {:item true}
-    ;; For properties that require React Node as parameter,
-    ;; use r/as-element to convert Reagent hiccup forms into React elements,
-    ;; or use r/create-element to directly instantiate element from React class (i.e. non-adapted React component).
-    [grid {:item true}
-     [chip
-      {:icon  (r/as-element [face])
-       :label "Icon element example, r/as-element"}]]]
 
-   [grid {:item true}
-    [date-picker {:value        @date-picker-state
-                  :render-input (react-component [props]
-                                  [text-field props])
-                  :on-change    (fn [value]
-                                  (reset! date-picker-state value))
-                  :input-format "MM/dd/yyyy"
-                  :label        "Date picker"}]]
-   [grid {:item true}
-    [autocomplete {:class (:text-field classes)
-                   :value @autocomplete-state
-                   :on-change (fn [new-value]
-                                (reset! autocomplete-state new-value))}]]])
+   [date-picker {:value        @date-picker-state
+                 :render-input (react-component [props]
+                                 [text-field props])
+                 :on-change    (fn [value]
+                                 (reset! date-picker-state value))
+                 :input-format "MM/dd/yyyy"
+                 :label        "Date picker"}]
+   [autocomplete {:class     (:text-field classes)
+                  :value     @autocomplete-state
+                  :on-change (fn [new-value]
+                               (reset! autocomplete-state new-value))}]])
+
+(def form (with-custom-styles form*))
 
 (defn main []
   ;; fragment
@@ -166,17 +163,12 @@
    [css-baseline]
    ;; localization-provider provides date handling utils to date and time pickers.
    ;; cljs-time-adapter is a date adapter that allows you to use cljs-time / goog.date date objects.
+   ;; styled-engine-provider is needed to ensure correct order of CSS injection until Material UI has completely migrated to emotion
    [localization-provider {:date-adapter cljs-time-adapter
                            :locale       DateTimeSymbols_en_US}
     [styles/theme-provider (styles/create-mui-theme custom-theme)
-     [grid
-      {:container       true
-       :direction       "row"
-       :justify-content "center"}
-      [grid
-       {:item true
-        :xs   6}
-       [(with-custom-styles form)]]]]]])
+     [styled-engine-provider {:inject-first true}
+      [form]]]]])
 
 (defn ^{:after-load true, :dev/after-load true}
   mount []
